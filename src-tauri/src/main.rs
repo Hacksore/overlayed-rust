e15c6da319359fa38be60cd6feb5a1f94cb0f908
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-09-22 16:52:20 -0500

    Working

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,7 @@
-use rpc_discord::{Command, DiscordIpc, DiscordIpcClient, Event, EventReceive};
+use rpc_discord::{Command, DiscordIpc, DiscordIpcClient, Event };
 use tauri::{AppHandle};
+
+use::tauri::Manager;
 
 // the payload type must implement `Serialize` and `Clone`.
 #[derive(Clone, serde::Serialize)]
@@ -7,22 +9,15 @@ struct Payload {
   client_id: String,
 }
 
-
-fn handle_message(event: EventReceive) {
-  // TODO: get a ref to the app so I can use that to all app.emit_*
-  // tauri::AppHandle
-  // app.emit_all("message", "{}");
-
-  println!("{:#?}", event);
-}
+const CHANNEL_ID: &str = "1019035649870934108";
 
 #[tauri::command(async)]
 async fn init_socket(app: AppHandle) {
   // access token from env
-  let access_token = dotenv::var("ACCESS_TOKEN").unwrap();
+  let access_token = dotenv::var("ACCESS_TOKEN").expect("Must provide access token");
 
   // client id from env
-  let client_id = dotenv::var("CLIENT_ID").unwrap();
+  let client_id = dotenv::var("CLIENT_ID").expect("Must define client id");
 
   // connect to discord client with overlayed id
   let mut client = DiscordIpcClient::new(&client_id)
@@ -38,24 +33,26 @@ async fn init_socket(app: AppHandle) {
     .ok();
 
   client
-    .emit(Event::speaking_start_event("1021507676871589939"))
+    .emit(Event::speaking_start_event(CHANNEL_ID))
     .await
     .ok();
 
   client
-    .emit(Event::speaking_stop_event("1021507676871589939"))
+    .emit(Event::speaking_stop_event(CHANNEL_ID))
     .await
     .ok();
 
-  client.handler(handle_message).await.ok();
-
-  println!("made it here");
+  client.handler(|e| {
+    app.emit_all("message", "{ hi: \"mom\"}").unwrap();    
+  }).await;
 }
 
 #[tokio::main]
 async fn main() {
   // TODO: get real auth at some point
   dotenv::dotenv().ok();
+
+  println!("CLIENT_ID: {}", dotenv::var("CLIENT_ID").unwrap());
 
   let app = tauri::Builder::default();
 
--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,24 @@ import Login from "./views/login";
 import Main from "./views/main";
 
 // When using the Tauri API npm package:
-import { invoke } from '@tauri-apps/api/tauri'
-// import { useEffect } from "react";
-// When using the Tauri global script (if not using the npm package)
-// Be sure to set `build.withGlobalTauri` in `tauri.conf.json` to true
-// @ts-ignore
-// const invoke = window.__TAURI__.invoke
+import { invoke } from "@tauri-apps/api/tauri";
+import { listen } from "@tauri-apps/api/event";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const listenForEvents = async () => {
+      return await listen("message", (event: any) => {
+        console.log(event);
+      });
+    };
 
-  // useEffect(() => {
-  //   const listenForEvents = async () => {
-  //     return await listen('speaking-start', (event: any) => {
-  //       console.log(event);
-  //     })
-  //   }
-
-  //   listenForEvents();
-  // })
+    listenForEvents();
+  }, []);
 
   return (
     <div>
-
-      <button onClick={() => invoke("init_socket", { S: "test"} )}>test</button>
+      <button onClick={() => invoke("init_socket", { S: "test" })}>test</button>
       <Header />
       <HashRouter>
         <Routes>
